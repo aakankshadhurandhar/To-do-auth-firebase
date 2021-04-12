21eb00610fbdf5bb1ae898e19040ccb8ef6598a6
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-04-12 17:01:15 +0530

    changes in env

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import 'firebase/auth';
 
 const app=firebase.initializeApp({
 
-    apiKey: "AIzaSyD2rgjteREtntdGgR15EhRhu1rIN6vrQXc",
-    authDomain: "photo-gallery-app-b9d09.firebaseapp.com",
-    projectId: "photo-gallery-app-b9d09",
-    storageBucket: "photo-gallery-app-b9d09.appspot.com",
-    messagingSenderId: "604624635043",
-    appId: "1:604624635043:web:14f89c8c27e82214476b16"
+    apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+    storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSENGER_SENDER_ID ,
+    appId:process.env.REACT_APP_FIREBASE_APP_ID
+
 
 })
 export default app;
